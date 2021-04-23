939e548ff86cfc044691ad619d9f155046636959
Hojoon Lee <hojoon.lee@skku.edu>
2021-04-23 10:14:46 +0000

    initial

---
+++ app/TestApp.cpp
@@ -0,0 +1,419 @@
+/*
+ * Copyright (C) 2011-2017 Intel Corporation. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *   * Neither the name of Intel Corporation nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <inttypes.h>
+#include <libgen.h>
+#include <pthread.h>
+#include <pwd.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/epoll.h>
+#include <sys/socket.h>
+
+#define SERVERBACKLOG 10
+#define NUM_DID_REQ_THR 4
+#define NUM_DRF_RECV_THR 4
+
+#define MAX_PATH FILENAME_MAX
+#define MAX_EVENTS 2000
+
+#include "TestApp.h"
+#include "TestEnclave_u.h"
+#include "ThreadPool.h"
+#include <sgx_urts.h>
+
+#define DID_REQ_PORT 8080
+#define DID_REQ_PORT 8080
+#define EVENTS_BUFF_SZ 256
+/* Global EID shared by multiple threads */
+sgx_enclave_id_t global_eid = 0;
+
+// EPOLL fd
+static int did_req_epoll_fd, drf_recv_epoll_fd;
+// For DID requests, and DRF receiving
+int did_req_sock, drf_recv_sock;
+
+typedef struct _sgx_errlist_t {
+  sgx_status_t err;
+  const char *msg;
+  const char *sug; /* Suggestion */
+} sgx_errlist_t;
+
+/* Error code returned by sgx_create_enclave */
+static sgx_errlist_t sgx_errlist[] = {
+    {SGX_ERROR_UNEXPECTED, "Unexpected error occurred.", NULL},
+    {SGX_ERROR_INVALID_PARAMETER, "Invalid parameter.", NULL},
+    {SGX_ERROR_OUT_OF_MEMORY, "Out of memory.", NULL},
+    {SGX_ERROR_ENCLAVE_LOST, "Power transition occurred.",
+     "Please refer to the sample \"PowerTransition\" for details."},
+    {SGX_ERROR_INVALID_ENCLAVE, "Invalid enclave image.", NULL},
+    {SGX_ERROR_INVALID_ENCLAVE_ID, "Invalid enclave identification.", NULL},
+    {SGX_ERROR_INVALID_SIGNATURE, "Invalid enclave signature.", NULL},
+    {SGX_ERROR_OUT_OF_EPC, "Out of EPC memory.", NULL},
+    {SGX_ERROR_NO_DEVICE, "Invalid Intel® Software Guard Extensions device.",
+     "Please make sure Intel® Software Guard Extensions module is enabled in "
+     "the BIOS, and install Intel® Software Guard Extensions driver "
+     "afterwards."},
+    {SGX_ERROR_MEMORY_MAP_CONFLICT, "Memory map conflicted.", NULL},
+    {SGX_ERROR_INVALID_METADATA, "Invalid enclave metadata.", NULL},
+    {SGX_ERROR_DEVICE_BUSY, "Intel® Software Guard Extensions device was busy.",
+     NULL},
+    {SGX_ERROR_INVALID_VERSION, "Enclave version was invalid.", NULL},
+    {SGX_ERROR_INVALID_ATTRIBUTE, "Enclave was not authorized.", NULL},
+    {SGX_ERROR_ENCLAVE_FILE_ACCESS, "Can't open enclave file.", NULL},
+};
+
+/* Check error conditions for loading enclave */
+void print_error_message(sgx_status_t ret) {
+  size_t idx = 0;
+  size_t ttl = sizeof sgx_errlist / sizeof sgx_errlist[0];
+
+  for (idx = 0; idx < ttl; idx++) {
+    if (ret == sgx_errlist[idx].err) {
+      if (NULL != sgx_errlist[idx].sug)
+        printf("Info: %s\n", sgx_errlist[idx].sug);
+      printf("Error: %s\n", sgx_errlist[idx].msg);
+      break;
+    }
+  }
+
+  if (idx == ttl)
+    printf("Error: Unexpected error occurred [0x%x].\n", ret);
+}
+
+/* Initialize the enclave:
+ *   Step 1: retrive the launch token saved by last transaction
+ *   Step 2: call sgx_create_enclave to initialize an enclave instance
+ *   Step 3: save the launch token if it is updated
+ */
+int initialize_enclave(void) {
+  char token_path[MAX_PATH] = {'\0'};
+  sgx_launch_token_t token = {0};
+  sgx_status_t ret = SGX_ERROR_UNEXPECTED;
+  int updated = 0;
+  /* Step 1: retrive the launch token saved by last transaction */
+
+  /* try to get the token saved in $HOME */
+  const char *home_dir = getpwuid(getuid())->pw_dir;
+  if (home_dir != NULL && (strlen(home_dir) + strlen("/") +
+                           sizeof(TOKEN_FILENAME) + 1) <= MAX_PATH) {
+    /* compose the token path */
+    strncpy(token_path, home_dir, strlen(home_dir));
+    strncat(token_path, "/", strlen("/"));
+    strncat(token_path, TOKEN_FILENAME, sizeof(TOKEN_FILENAME) + 1);
+  } else {
+    /* if token path is too long or $HOME is NULL */
+    strncpy(token_path, TOKEN_FILENAME, sizeof(TOKEN_FILENAME));
+  }
+
+  FILE *fp = fopen(token_path, "rb");
+  if (fp == NULL && (fp = fopen(token_path, "wb")) == NULL) {
+    printf("Warning: Failed to create/open the launch token file \"%s\".\n",
+           token_path);
+  }
+  printf("token_path: %s\n", token_path);
+  if (fp != NULL) {
+    /* read the token from saved file */
+    size_t read_num = fread(token, 1, sizeof(sgx_launch_token_t), fp);
+    if (read_num != 0 && read_num != sizeof(sgx_launch_token_t)) {
+      /* if token is invalid, clear the buffer */
+      memset(&token, 0x0, sizeof(sgx_launch_token_t));
+      printf("Warning: Invalid launch token read from \"%s\".\n", token_path);
+    }
+  }
+
+  /* Step 2: call sgx_create_enclave to initialize an enclave instance */
+  /* Debug Support: set 2nd parameter to 1 */
+
+  ret = sgx_create_enclave(TESTENCLAVE_FILENAME, SGX_DEBUG_FLAG, &token,
+                           &updated, &global_eid, NULL);
+
+  if (ret != SGX_SUCCESS) {
+    print_error_message(ret);
+    if (fp != NULL)
+      fclose(fp);
+
+    return -1;
+  }
+
+  /* Step 3: save the launch token if it is updated */
+
+  if (updated == FALSE || fp == NULL) {
+    /* if the token is not updated, or file handler is invalid, do not perform
+     * saving */
+    if (fp != NULL)
+      fclose(fp);
+    return 0;
+  }
+
+  /* reopen the file with write capablity */
+  fp = freopen(token_path, "wb", fp);
+  if (fp == NULL)
+    return 0;
+  size_t write_num = fwrite(token, 1, sizeof(sgx_launch_token_t), fp);
+  if (write_num != sizeof(sgx_launch_token_t))
+    printf("Warning: Failed to save launch token to \"%s\".\n", token_path);
+  fclose(fp);
+
+  return 0;
+}
+
+/* OCall functions */
+void uprint(const char *str) {
+  /* Proxy/Bridge will check the length and null-terminate
+   * the input string to prevent buffer overflow.
+   */
+  printf("%s", str);
+  fflush(stdout);
+}
+
+void usgx_exit(int reason) {
+  printf("usgx_exit: %d\n", reason);
+  exit(reason);
+}
+
+void *thread_test_func(void *p) {
+  new_thread_func(global_eid);
+  return NULL;
+}
+
+int ucreate_thread() {
+  pthread_t thread;
+  int res = pthread_create(&thread, NULL, thread_test_func, NULL);
+  return res;
+}
+
+void hello() { printf("Hello\n"); }
+
+int prepare_socket(int port) {
+
+  int sockfd;
+  struct sockaddr_in serveraddr;
+
+  serveraddr.sin_family = AF_INET;
+  serveraddr.sin_port = htons(port);
+  serveraddr.sin_addr.s_addr = INADDR_ANY;
+
+  if ((sockfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
+    perror("socket(2) failed");
+    exit(EXIT_FAILURE);
+  }
+
+  if (bind(sockfd, (const struct sockaddr *)&serveraddr, sizeof(serveraddr)) <
+      0) {
+    perror("bind(2) failed");
+    exit(EXIT_FAILURE);
+  }
+
+  if (listen(sockfd, SERVERBACKLOG) < 0) {
+    perror("listen(2) failed");
+    exit(EXIT_FAILURE);
+  }
+  return sockfd;
+}
+
+int prepare_epoll(int sock) {
+  int epoll_fd;
+  struct epoll_event epevent;
+  epevent.events = EPOLLIN | EPOLLET;
+  epevent.data.fd = sock;
+  if ((epoll_fd = epoll_create(1)) < 0) {
+    perror("epoll_create(2) failed");
+    exit(EXIT_FAILURE);
+  }
+
+  // Epoll
+  epevent.events = EPOLLIN | EPOLLET;
+  epevent.data.fd = did_req_sock;
+
+  if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, did_req_sock, &epevent) < 0) {
+    perror("epoll_ctl(2) failed on main server socket");
+    exit(EXIT_FAILURE);
+  }
+
+  return epoll_fd;
+}
+
+int accept_new_client(int sock, int epoll_fd) {
+
+  int clientsock;
+  struct sockaddr_in addr;
+  socklen_t addrlen = sizeof(addr);
+  if ((clientsock = accept(sock, (struct sockaddr *)&addr, &addrlen)) < 0) {
+    return -1;
+  }
+
+  char ip_buff[INET_ADDRSTRLEN + 1];
+  if (inet_ntop(AF_INET, &addr.sin_addr, ip_buff, sizeof(ip_buff)) == NULL) {
+    close(clientsock);
+    return -1;
+  }
+
+  printf("*** [%p] Client connected from %s:%" PRIu16 "\n",
+         (void *)pthread_self(), ip_buff, ntohs(addr.sin_port));
+
+  struct epoll_event epevent;
+  epevent.events = EPOLLIN | EPOLLET;
+  epevent.data.fd = clientsock;
+
+  if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, clientsock, &epevent) < 0) {
+    perror("epoll_ctl(2) failed attempting to add new client");
+    close(clientsock);
+    return -1;
+  }
+
+  return 0;
+}
+
+int handle_request(int clientfd) {
+  char readbuff[512];
+  struct sockaddr_in addr;
+  socklen_t addrlen = sizeof(addr);
+  ssize_t n;
+
+  if ((n = recv(clientfd, readbuff, sizeof(readbuff) - 1, 0)) < 0) {
+    return -1;
+  }
+
+  if (n == 0) {
+    return 0;
+  }
+
+  readbuff[n] = '\0';
+
+  if (getpeername(clientfd, (struct sockaddr *)&addr, &addrlen) < 0) {
+    return -1;
+  }
+
+  char ip_buff[INET_ADDRSTRLEN + 1];
+  if (inet_ntop(AF_INET, &addr.sin_addr, ip_buff, sizeof(ip_buff)) == NULL) {
+    return -1;
+  }
+
+  printf("*** [%p] [%s:%" PRIu16 "] -> server: %s", (void *)pthread_self(),
+         ip_buff, ntohs(addr.sin_port), readbuff);
+
+  ssize_t sent;
+  if ((sent = send(clientfd, readbuff, n, 0)) < 0) {
+    return -1;
+  }
+
+  readbuff[sent] = '\0';
+
+  printf("*** [%p] server -> [%s:%" PRIu16 "]: %s", (void *)pthread_self(),
+         ip_buff, ntohs(addr.sin_port), readbuff);
+
+  return 0;
+}
+
+void *did_req_worker_thread(int sock, int epoll_fd) {
+  struct epoll_event *events =
+      (struct epoll_event *)malloc(sizeof(*events) * EVENTS_BUFF_SZ);
+  if (events == NULL) {
+    perror("malloc(3) failed when attempting to allocate events buffer");
+    pthread_exit(NULL);
+  }
+
+  int events_cnt;
+  while ((events_cnt =
+              epoll_wait(did_req_epoll_fd, events, EVENTS_BUFF_SZ, -1)) > 0) {
+    int i;
+    for (i = 0; i < events_cnt; i++) {
+      assert(events[i].events & EPOLLIN);
+
+      if (events[i].data.fd == did_req_sock) {
+        if (accept_new_client(sock, epoll_fd) == -1) {
+          fprintf(stderr, "Error accepting new client: %s\n", strerror(errno));
+        }
+      } else {
+        if (handle_request(events[i].data.fd) == -1) {
+          fprintf(stderr, "Error handling request: %s\n", strerror(errno));
+        }
+      }
+    }
+  }
+
+  if (events_cnt == 0) {
+    fprintf(stderr,
+            "epoll_wait(2) returned 0, but timeout was not specified...?");
+  } else {
+    perror("epoll_wait(2) error");
+  }
+
+  free(events);
+
+  return NULL;
+}
+
+int main(int argc, char *argv[]) {
+  int sgxStatus;
+
+  ThreadPool didQueryPool(NUM_DID_REQ_THR);
+  ThreadPool didDocFetchPool(NUM_DRF_RECV_THR);
+  /* Changing dir to where the executable is.*/
+  // char absolutePath[MAX_PATH];
+  // struct epoll_event epevent;
+
+  /* Initialize the enclave */
+  if (initialize_enclave() < 0)
+    return 1;
+
+  // Initialize thread pools
+  didQueryPool.init();
+  didDocFetchPool.init();
+
+  // Create, bind, listen
+  did_req_sock = prepare_socket(DID_REQ_PORT);
+  if (did_req_sock == -1)
+    return 0;
+
+  did_req_epoll_fd = prepare_epoll(did_req_sock);
+  if (did_req_epoll_fd == -1)
+    return 0;
+
+  char c;
+  while (1) {
+    didQueryPool.submit(did_req_worker_thread, did_req_sock, did_req_epoll_fd);
+  }
+
+  didQueryPool.shutdown();
+  didDocFetchPool.shutdown();
+  sgx_destroy_enclave(global_eid);
+
+  return 0;
+}
---
+++ enclave/TestEnclave.cpp
@@ -0,0 +1,526 @@
+/*
+ * Copyright (C) 2011-2017 Intel Corporation. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *   * Neither the name of Intel Corporation nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+#include <stdarg.h>
+#include <stdio.h> /* vsnprintf */
+
+#include "TestEnclave.h"
+#include "TestEnclave_t.h" /* print_string */
+#include "tSgxSSL_api.h"
+
+#include <openssl/bn.h>
+#include <openssl/ec.h>
+#include <openssl/err.h>
+#include <openssl/evp.h>
+#include <openssl/rand.h>
+#include <openssl/rsa.h>
+
+#include <openssl/ssl.h>
+#include <openssl/x509.h>
+
+#define ADD_ENTROPY_SIZE 32
+
+/*
+ * printf:
+ *   Invokes OCALL to display the enclave buffer to the terminal.
+ */
+void printf(const char *fmt, ...) {
+  char buf[BUFSIZ] = {'\0'};
+  va_list ap;
+  va_start(ap, fmt);
+  vsnprintf(buf, BUFSIZ, fmt, ap);
+  va_end(ap);
+  uprint(buf);
+}
+
+// typedef void CRYPTO_RWLOCK;
+
+// struct evp_pkey_st {
+//     int type;
+//     int save_type;
+//     int references;
+//     const EVP_PKEY_ASN1_METHOD *ameth;
+//     ENGINE *engine;
+//     union {
+//         char *ptr;
+// # ifndef OPENSSL_NO_RSA
+//         struct rsa_st *rsa;     /* RSA */
+// # endif
+// # ifndef OPENSSL_NO_DSA
+//         struct dsa_st *dsa;     /* DSA */
+// # endif
+// # ifndef OPENSSL_NO_DH
+//         struct dh_st *dh;       /* DH */
+// # endif
+// # ifndef OPENSSL_NO_EC
+//         struct ec_key_st *ec;   /* ECC */
+// # endif
+//     } pkey;
+//     int save_parameters;
+//     STACK_OF(X509_ATTRIBUTE) *attributes; /* [ 0 ] */
+//     CRYPTO_RWLOCK *lock;
+// } /* EVP_PKEY */ ;
+
+// void rsa_key_gen()
+// {
+// 	BIGNUM *bn = BN_new();
+// 	if (bn == NULL) {
+// 		printf("BN_new failure: %ld\n", ERR_get_error());
+// 	    return;
+// 	}
+// 	int ret = BN_set_word(bn, RSA_F4);
+//     if (!ret) {
+//        	printf("BN_set_word failure\n");
+// 	    return;
+// 	}
+
+// 	RSA *keypair = RSA_new();
+// 	if (keypair == NULL) {
+// 		printf("RSA_new failure: %ld\n", ERR_get_error());
+// 	    return;
+// 	}
+// 	ret = RSA_generate_key_ex(keypair, 4096, bn, NULL);
+// 	if (!ret) {
+//         printf("RSA_generate_key_ex failure: %ld\n", ERR_get_error());
+// 	    return;
+// 	}
+
+// 	EVP_PKEY *evp_pkey = EVP_PKEY_new();
+// 	if (evp_pkey == NULL) {
+// 		printf("EVP_PKEY_new failure: %ld\n", ERR_get_error());
+// 		return;
+// 	}
+// 	EVP_PKEY_assign_RSA(evp_pkey, keypair);
+
+// 	// public key - string
+// 	int len = i2d_PublicKey(evp_pkey, NULL);
+// 	unsigned char *buf = (unsigned char *) malloc (len + 1);
+// 	unsigned char *tbuf = buf;
+// 	i2d_PublicKey(evp_pkey, &tbuf);
+
+// 	// print public key
+// 	printf ("{\"public\":\"");
+// 	int i;
+// 	for (i = 0; i < len; i++) {
+// 	    printf("%02x", (unsigned char) buf[i]);
+// 	}
+// 	printf("\"}\n");
+
+// 	free(buf);
+
+// 	// private key - string
+// 	len = i2d_PrivateKey(evp_pkey, NULL);
+// 	buf = (unsigned char *) malloc (len + 1);
+// 	tbuf = buf;
+// 	i2d_PrivateKey(evp_pkey, &tbuf);
+
+// 	// print private key
+// 	printf ("{\"private\":\"");
+// 	for (i = 0; i < len; i++) {
+// 	    printf("%02x", (unsigned char) buf[i]);
+// 	}
+// 	printf("\"}\n");
+
+// 	free(buf);
+
+// 	BN_free(bn);
+
+// 	EVP_PKEY_free(evp_pkey);
+
+// 	if (evp_pkey->pkey.ptr != NULL) {
+// 	  RSA_free(keypair);
+// 	}
+// }
+
+// void ec_key_gen()
+// {
+// 	unsigned char entropy_buf[ADD_ENTROPY_SIZE] = {0};
+
+// 	RAND_add(entropy_buf, sizeof(entropy_buf), ADD_ENTROPY_SIZE);
+// 	RAND_seed(entropy_buf, sizeof(entropy_buf));
+
+// 	EC_KEY * ec = NULL;
+//     int eccgroup;
+//     eccgroup = OBJ_txt2nid("secp384r1");
+//     ec = EC_KEY_new_by_curve_name(eccgroup);
+//     if (ec == NULL) {
+//     	printf("EC_KEY_new_by_curve_name failure: %ld\n", ERR_get_error());
+// 	    return;
+//     }
+
+// 	EC_KEY_set_asn1_flag(ec, OPENSSL_EC_NAMED_CURVE);
+
+// 	int ret = EC_KEY_generate_key(ec);
+// 	if (!ret) {
+//         printf("EC_KEY_generate_key failure\n");
+// 	    return;
+// 	}
+
+// 	EVP_PKEY *ec_pkey = EVP_PKEY_new();
+// 	if (ec_pkey == NULL) {
+// 		printf("EVP_PKEY_new failure: %ld\n", ERR_get_error());
+// 		return;
+// 	}
+// 	EVP_PKEY_assign_EC_KEY(ec_pkey, ec);
+
+// 	// public key - string
+// 	int len = i2d_PublicKey(ec_pkey, NULL);
+// 	unsigned char *buf = (unsigned char *) malloc (len + 1);
+// 	unsigned char *tbuf = buf;
+// 	i2d_PublicKey(ec_pkey, &tbuf);
+
+// 	// print public key
+// 	printf ("{\"public\":\"");
+// 	int i;
+// 	for (i = 0; i < len; i++) {
+// 	    printf("%02x", (unsigned char) buf[i]);
+// 	}
+// 	printf("\"}\n");
+
+// 	free(buf);
+
+// 	// private key - string
+// 	len = i2d_PrivateKey(ec_pkey, NULL);
+// 	buf = (unsigned char *) malloc (len + 1);
+// 	tbuf = buf;
+// 	i2d_PrivateKey(ec_pkey, &tbuf);
+
+// 	// print private key
+// 	printf ("{\"private\":\"");
+// 	for (i = 0; i < len; i++) {
+// 	    printf("%02x", (unsigned char) buf[i]);
+// 	}
+// 	printf("\"}\n");
+
+// 	free(buf);
+
+// 	EVP_PKEY_free(ec_pkey);
+// 	if (ec_pkey->pkey.ptr != NULL) {
+// 	  EC_KEY_free(ec);
+// 	}
+// }
+
+// int vprintf_cb(Stream_t stream, const char * fmt, va_list arg)
+// {
+// 	char buf[BUFSIZ] = {'\0'};
+
+// 	int res = vsnprintf(buf, BUFSIZ, fmt, arg);
+// 	if (res >=0) {
+// 		sgx_status_t sgx_ret = uprint((const char *) buf);
+// 		TEST_CHECK(sgx_ret);
+// 	}
+// 	return res;
+// }
+
+// extern "C" int CRYPTO_set_mem_functions(
+//         void *(*m)(size_t, const char *, int),
+//         void *(*r)(void *, size_t, const char *, int),
+//         void (*f)(void *, const char *, int));
+// void* priv_malloc(size_t size, const char *file, int line)
+// {
+// 	void* addr = malloc(size);
+
+// 	printf("[malloc:%s:%d] size: %d, addr: %p\n", file, line, size, addr);
+
+// 	return addr;
+// }
+// void* priv_realloc(void* old_addr, size_t new_size, const char *file, int
+// line)
+// {
+// 	void* new_addr = realloc(old_addr, new_size);
+
+// 	printf("[realloc:%s:%d] old_addr: %p, new_size: %d, new_addr: %p\n",
+// file, line, old_addr, new_size, new_addr);
+
+// 	return new_addr;
+// }
+// void priv_free(void* addr, const char *file, int line)
+// {
+// 	printf("[free:%s:%d] addr: %p\n", file, line, addr);
+
+// 	free(addr);
+// }
+
+// void t_sgxssl_call_apis()
+// {
+//     int ret = 0;
+
+//     printf("Start tests\n");
+
+//     SGXSSLSetPrintToStdoutStderrCB(vprintf_cb);
+
+//     //CRYPTO_set_mem_functions(priv_malloc, priv_realloc, priv_free);
+
+//     // Initialize SGXSSL crypto
+//     OPENSSL_init_crypto(0, NULL);
+
+//     rsa_key_gen();
+//     printf("test rsa_key_gen completed\n");
+
+//     ec_key_gen();
+// 	printf("test ec_key_gen completed\n");
+
+//     ret = rsa_test();
+//     if (ret != 0)
+//     {
+//     	printf("test rsa_test returned error %d\n", ret);
+//     	exit(ret);
+//     }
+// 	printf("test rsa_test completed\n");
+
+// 	ret = ec_test();
+// 	if (ret != 0)
+//     {
+//     	printf("test ec_test returned error %d\n", ret);
+//     	exit(ret);
+//     }
+// 	printf("test ec_test completed\n");
+
+// 	ret = ecdh_test();
+// 	if (ret != 0)
+//     {
+//     	printf("test ecdh_test returned error %d\n", ret);
+//     	exit(ret);
+//     }
+// 	printf("test ecdh_test completed\n");
+
+// 	ret = ecdsa_test();
+// 	if (ret != 0)
+//     {
+//     	printf("test ecdsar_test returned error %d\n", ret);
+//     	exit(ret);
+//     }
+// 	printf("test ecdsa_test completed\n");
+
+// 	ret = bn_test();
+// 	if (ret != 0)
+//     {
+//     	printf("test bn_test returned error %d\n", ret);
+//     	exit(ret);
+//     }
+// 	printf("test bn_test completed\n");
+
+// 	ret = dh_test();
+// 	if (ret != 0)
+//     {
+//     	printf("test dh_test returned error %d\n", ret);
+//     	exit(ret);
+//     }
+// 	printf("test dh_test completed\n");
+
+// 	ret = sha256_test();
+// 	if (ret != 0)
+//     {
+//     	printf("test sha256_test returned error %d\n", ret);
+//     	exit(ret);
+//     }
+// 	printf("test sha256_test completed\n");
+
+// 	ret = sha1_test();
+// 	if (ret != 0)
+//     {
+//     	printf("test sha1_test returned error %d\n", ret);
+//     	exit(ret);
+//     }
+// 	printf("test sha1_test completed\n");
+
+// 	ret = threads_test();
+// 	if (ret != 0)
+//     {
+//     	printf("test threads_test returned error %d\n", ret);
+//     	exit(ret);
+//     }
+// 	printf("test threads_test completed\n");
+
+// 	ret = threads_test();
+// 	if (ret != 0)
+//     {
+//     	printf("test threads_test returned error %d\n", ret);
+//     	exit(ret);
+//     }
+// 	printf("test threads_test completed\n");
+
+// }
+
+static void init_openssl() {
+  OpenSSL_add_ssl_algorithms();
+  OpenSSL_add_all_ciphers();
+  SSL_load_error_strings();
+}
+
+static SSL_CTX *create_context() {
+  const SSL_METHOD *method;
+  SSL_CTX *ctx;
+
+  method = TLSv1_2_method();
+
+  ctx = SSL_CTX_new(method);
+  if (!ctx) {
+    printf("Unable to create SSL context");
+    exit(EXIT_FAILURE);
+  }
+  return ctx;
+}
+
+static EVP_PKEY *generatePrivateKey() {
+  EVP_PKEY *pkey = NULL;
+  EVP_PKEY_CTX *pctx = EVP_PKEY_CTX_new_id(EVP_PKEY_RSA, NULL);
+  EVP_PKEY_keygen_init(pctx);
+  EVP_PKEY_CTX_set_rsa_keygen_bits(pctx, 2048);
+  EVP_PKEY_keygen(pctx, &pkey);
+  return pkey;
+}
+
+static X509 *generateCertificate(EVP_PKEY *pkey) {
+  X509 *x509 = X509_new();
+  X509_set_version(x509, 2);
+  ASN1_INTEGER_set(X509_get_serialNumber(x509), 0);
+  X509_gmtime_adj(X509_get_notBefore(x509), 0);
+  X509_gmtime_adj(X509_get_notAfter(x509), (long)60 * 60 * 24 * 365);
+  X509_set_pubkey(x509, pkey);
+
+  X509_NAME *name = X509_get_subject_name(x509);
+  X509_NAME_add_entry_by_txt(name, "C", MBSTRING_ASC,
+                             (const unsigned char *)"US", -1, -1, 0);
+  X509_NAME_add_entry_by_txt(name, "CN", MBSTRING_ASC,
+                             (const unsigned char *)"YourCN", -1, -1, 0);
+  X509_set_issuer_name(x509, name);
+  X509_sign(x509, pkey, EVP_md5());
+  return x509;
+}
+
+// static void configure_context(SSL_CTX *ctx)
+// {
+//     EVP_PKEY *pkey = generatePrivateKey();
+// 	X509 *x509 = generateCertificate(pkey);
+
+// 	SSL_CTX_use_certificate(ctx, x509);
+// 	SSL_CTX_set_default_passwd_cb(ctx, password_cb);
+// 	SSL_CTX_use_PrivateKey(ctx, pkey);
+
+// 	RSA *rsa=RSA_generate_key(512, RSA_F4, NULL, NULL);
+// 	SSL_CTX_set_tmp_rsa(ctx, rsa);
+// 	RSA_free(rsa);
+
+// 	SSL_CTX_set_verify(ctx, SSL_VERIFY_NONE, 0);
+// }
+
+// static int create_socket_server(int port)
+// {
+//     int s, optval = 1;
+//     struct sockaddr_in addr;
+
+//     addr.sin_family = AF_INET;
+//     addr.sin_port = htons(port);
+//     addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+//     s = socket(AF_INET, SOCK_STREAM, 0);
+//     if (s < 0) {
+//     	printf("sgx_socket");
+// 		exit(EXIT_FAILURE);
+//     }
+//     if (setsockopt(s, SOL_SOCKET, SO_REUSEADDR, (const void *)&optval,
+//     sizeof(int)) < 0) {
+// 		printf("sgx_setsockopt");
+// 		exit(EXIT_FAILURE);
+//     }
+//     if (bind(s, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
+//     	printf("sgx_bind");
+// 		exit(EXIT_FAILURE);
+//     }
+//     if (listen(s, 128) < 0) {
+//     	printf("sgx_listen");
+// 		exit(EXIT_FAILURE);
+//     }
+//     return s;
+// }
+
+void ecall_init_tls() {
+  // OpenSSL_add_ssl_algorithms();
+  // OpenSSL_add_all_ciphers();
+  // SSL_load_error_strings();
+}
+
+void ecall_prepare_tls_context() {}
+
+void ecall_start_tls_server(void) {
+  int sock;
+  SSL_CTX *ctx;
+  init_openssl();
+  // BIO_new_ssl_connect(NULL);
+
+  // printf("OPENSSL Version = %s", SSLeay_version(SSLEAY_VERSION));
+  // init_openssl();
+  // ctx = create_context();
+  // configure_context(ctx);
+
+  // sock = create_socket_server(4433);
+  // if (sock < 0) {
+  //   printe("create_socket_client");
+  //   exit(EXIT_FAILURE);
+  // }
+
+  // /* Handle SSL/TLS connections */
+  // while (1) {
+  //   struct sockaddr_in addr;
+  //   int len = sizeof(addr);
+  //   SSL *cli;
+  //   unsigned char read_buf[1024];
+  //   int r = 0;
+  //   printf("Wait for new connection...");
+  //   int client = accept(sock, (struct sockaddr *)&addr, &len);
+  //   if (client < 0) {
+  //     printe("Unable to accept");
+  //     exit(EXIT_FAILURE);
+  //   }
+
+  //   cli = SSL_new(ctx);
+  //   SSL_set_fd(cli, client);
+  //   if (SSL_accept(cli) <= 0) {
+  //     printe("SSL_accept");
+  //     exit(EXIT_FAILURE);
+  //   }
+
+  //   printf("ciphersuit: %s", SSL_get_current_cipher(cli)->name);
+  //   /* Receive buffer from TLS server */
+  //   r = SSL_read(cli, read_buf, sizeof(read_buf));
+  //   printf("read_buf: length = %d : %s", r, read_buf);
+  //   memset(read_buf, 0, sizeof(read_buf));
+
+  //   printf("Close SSL/TLS client");
+  //   SSL_free(cli);
+  //   sgx_close(client);
+  // }
+
+  // sgx_close(sock);
+  // SSL_CTX_free(ctx);
+  // cleanup_openssl();
+}
